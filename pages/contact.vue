b51e957c4693fd862d8c832c9a69a5c0790f589b
JM Mac <johnmarkmancol@gmail.com>
2022-09-04 00:53:26 +0800

    development page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <h1>Contact</h1>
+    <div class="normal-page">
+        <h1>Contact Me</h1>
+        <div class="div" style="display:flex;align-items:center;height:77vh">
+            <center style="width: 100%;">
+                <fa :icon="['fa', 'code']" style="font-size:8em" />
+                <h4>This page is under development.</h4>
+            </center>
+        </div>
     </div>
 </template>
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <h1>Gallery</h1>
+    <div class="normal-page">
+        <h1>Arts & Gallery</h1>
+        <div class="div" style="display:flex;align-items:center;height:77vh">
+            <center style="width: 100%;">
+                <fa :icon="['fa', 'code']" style="font-size:8em" />
+                <h4>This page is under development.</h4>
+            </center>
+        </div>
     </div>
 </template>
