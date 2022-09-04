3b91c14880574e01a5522809bb401bd284230aa5
JM Mac <johnmarkmancol@gmail.com>
2022-09-04 15:09:09 +0800

    gallery and contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,9 +3,64 @@
         <h1>Contact Me</h1>
         <div class="div" style="display:flex;align-items:center;height:77vh">
             <center style="width: 100%;">
-                <fa :icon="['fa', 'code']" style="font-size:8em" />
-                <h4>This page is under development.</h4>
+                <h3>Reach me out!</h3>
+                <div class="social-media">
+                    <a href="mailto: johnmarkmancol@gmail.com" target="_blank">
+                        <fa :icon="['fa', 'envelope']" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/john-mark-mancol-441b30b3/" target="_blank">
+                        <fa :icon="['fa-brands', 'linkedin']" />
+                    </a>
+                    <a href="https://github.com/KaitoJM" target="_blank">
+                        <fa :icon="['fa-brands', 'github']" />
+                    </a>
+                    <a href="https://www.pinterest.ph/jlocnam" target="_blank">
+                        <fa :icon="['fa-brands', 'square-pinterest']" />
+                    </a>
+                </div>
+                <br />
+                <h3>Social media</h3>
+                <div class="social-media">
+                    <a href="https://www.facebook.com/Kaito.JM" target="_blank">
+                        <fa :icon="['fa-brands', 'square-facebook']" />
+                    </a>
+                    <a href="https://twitter.com/Jan_Locnam" target="_blank">
+                        <fa :icon="['fa-brands', 'square-twitter']" />
+                    </a>
+                    <a href="https://www.instagram.com/kidjm04" target="_blank">
+                        <fa :icon="['fa-brands', 'square-instagram']" />
+                    </a>
+                </div>
             </center>
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.social-media {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+
+    //mobile
+    @media(max-width: 960px) {
+        gap: 15px;
+    }
+
+    a {
+        font-size: 6em;
+        display: block;
+        transition: transform 0.5s;
+
+        &:hover {
+            transform: scale(1.1);
+        }
+
+        //mobile
+        @media(max-width: 960px) {
+            font-size: 3em;
+        }
+    }
+}
+</style>
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,11 +1,35 @@
 <template>
     <div class="normal-page">
         <h1>Arts & Gallery</h1>
-        <div class="div" style="display:flex;align-items:center;height:77vh">
-            <center style="width: 100%;">
-                <fa :icon="['fa', 'code']" style="font-size:8em" />
-                <h4>This page is under development.</h4>
-            </center>
+        <div class="gallery-grid">
+            <img src="~/static/images/gallery/batch2011.webp" alt="">
+            <img src="~/static/images/gallery/compass.webp" alt="">
+            <img src="~/static/images/gallery/carpe-diem.webp" alt="">
+            <img src="~/static/images/gallery/engine.webp" alt="">
+            <img src="~/static/images/gallery/halaba-atop.webp" alt="">
+            <img src="~/static/images/gallery/talkischeap.webp" alt="">
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.gallery-grid {
+    display: flex;
+    align-items: flex-start;
+    width: 100%;
+    gap: 15px;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    img {
+        width: 23%;
+        object-fit: cover;
+        border-radius: 10px;
+
+        //mobile
+        @media(max-width: 960px) {
+            width: 48%;
+        }
+    }
+}
+</style>
