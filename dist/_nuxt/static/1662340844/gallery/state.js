1a3e1bf297f0be55f8ca53b4abb46e2959976478
JM Mac <johnmarkmancol@gmail.com>
2022-09-06 11:16:12 +0800

    gitignore

--- dist/_nuxt/static/1662340844/gallery/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662340844",layout:"default",error:a,state:{global:{background_properies:{"background-image":"none"}}},serverRendered:true,routePath:"\u002Fgallery",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
