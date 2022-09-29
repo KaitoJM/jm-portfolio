a8a81f0316713f41998e5b0fb0851157028bed2d
JM Mac <johnmarkmancol@gmail.com>
2022-09-30 00:50:27 +0800

    art link

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -2,13 +2,20 @@
     <div class="normal-page">
         <h1>Arts & Gallery</h1>
         <div class="gallery-grid">
-            <div class="item" v-for="(image, indx) in arts" :key="'art-' + indx">
+            <a 
+                :href="image.link"  
+                target="_blank" 
+                class="item" 
+                v-for="(image, indx) in arts" 
+                :key="'art-' + indx"
+                @click="clickImage(image)"
+            >
                 <img :src="image.img" alt="">
                 <div class="img-detail">
                     <strong>{{ image.name }}</strong>
                     <p v-if="image.summary">{{ image.summary }}</p>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 </template>
@@ -21,30 +28,37 @@ export default {
                 {
                     img: "/images/gallery/batch2011.webp",
                     name: "RLMSF Batch 2011",
+                    link: "https://www.pinterest.ph/pin/768215648938820479/?nic_v3=1a3am2z8R",
                 },
                 {
                     img: "/images/gallery/compass.webp",
                     name: "Traveler",
+                    link: "https://www.pinterest.ph/pin/768215648947867914/?nic_v3=1a3am2z8R"
                 },
                 {
                     img: "/images/gallery/carpe-diem.webp",
                     name: "Carpe Diem",
+                    link: "https://www.pinterest.ph/pin/768215648938820381/?nic_v3=1a3am2z8R"
                 },
                 {
                     img: "/images/gallery/engine.webp",
                     name: "Engine T-shirt Print",
+                    link: "https://www.pinterest.ph/pin/768215648944815853/?nic_v3=1a3am2z8R"
                 },
                 {
                     img: "/images/gallery/halaba-atop.webp",
                     name: "Halaba-Atop",
+                    link: "https://www.pinterest.ph/pin/768215648944815952/?nic_v3=1a3am2z8R"
                 },
                 {
                     img: "/images/gallery/talkischeap.webp",
                     name: "Talk is Cheap",
+                    link: "https://www.pinterest.ph/pin/768215648944815888/?nic_v3=1a3am2z8R"
                 },
                 {
                     img: "/images/gallery/onepiececap.webp",
                     name: "Back at it Again",
+                    link: "https://www.pinterest.ph/pin/768215648953200609/?nic_v3=1a3am2z8R"
                 },
             ]
         }
@@ -54,6 +68,14 @@ export default {
 			title: "Gallery - Juan Marcus",
 		}
 	},
+    methods: {
+        clickImage(img) {
+            this.$gtm.push({event: 'art_view', ...{
+                pinterest_link: img.link,
+                image_name: img.name
+            }})
+        }
+    }
 }
 </script>
 
@@ -75,6 +97,9 @@ export default {
     }
 
     .item {
+        display: block;
+        text-decoration: none;
+
         img {
             width: 100%;
             object-fit: cover;
